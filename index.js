cd13892c454366b4a7da387b106328bb1ccb731c
John O'Connor <oconnojb@gmail.com>
2019-03-09 21:59:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(selector) {
+  let aule = document.querySelector(selector);
+  return aule;
+}
+
+function nestedTarget() {
+  let aule = document.querySelector('#nested');
+  let melko = aule.querySelector('.target');
+  return melko;
+}
+
+function increaseRankBy(n) {
+  let aule = document.querySelectorAll('.ranked-list');
+
+  for (var i = 0; i < aule.length; i++) {
+    let melko = aule[i].getElementsByTagName('li');
+
+    for (var a = 0; a < melko.length; a++) {
+      melko[a].innerHTML = parseInt(melko[a].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  let aule = document.getElementById('grand-node');
+
+  while (aule.querySelector('div') != null) {
+    aule = aule.querySelector('div');
+  }
+
+  return aule;
+}
